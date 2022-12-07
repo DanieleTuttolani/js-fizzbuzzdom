0b4980d10d0383f9b5d5c1a08591cc7c07afba41
DanieleTuttolani <danieletuttolani2@gmail.com>
2022-12-07 14:39:56 +0100

    first commit

---
+++ script/script.js
@@ -0,0 +1,6 @@
+/*1 prendo i target/
+2. creo un ciclo da 1 a 100/ 
+3 sostituisco i multipli di 3 con fizz
+4- sostituisco i multipli di 5 con buzz
+5- sostituisco i multipli di 3 e 5 con fizzbuzz
+6-prendo i dati e stampo nel documento*/
